c6ee915153fb6160661e3b1ff4bacb5140fbdf95
Rachel Rohrbach <rachel.rohrbach@gmail.com>
2020-03-18 20:44:41 -0700

    created todo model

---
+++ models/todo.js
@@ -0,0 +1,10 @@
+module.exports = (sequelize, DataTypes) => 
+sequelize.define(`Todo`, {
+    text: {
+        type: DataTypes.STRING
+    },
+    isComplete: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false
+    }
+});
